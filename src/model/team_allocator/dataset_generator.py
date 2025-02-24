ae2e3387584b5d36c5df3a106ce473c0871a56a9
Karl-Louie <chthollyc@gmail.com>
2025-02-24 23:01:33 +0800

    task classifier

--- src/model/team_allocator/dataset_generator.py
+++ src/model/team_allocator/dataset_generator.py
@@ -4,12 +4,12 @@ from datetime import datetime, timedelta
 
 # define necessary dictionaries
 department_teams = {
-    1: [1, 2, 3, 4, 5, 6, 37],
-    2: [7, 8, 9, 10, 11, 12, 38],
-    3: [13, 14, 15, 16, 17, 18, 39],
-    4: [19, 20, 21, 22, 23, 24, 40],
-    5: [25, 26, 27, 28, 29, 30, 41],
-    6: [31, 32, 33, 34, 35, 36, 42],
+    1: [1, 2, 3, 4, 5, 6, 37],          #Catering
+    2: [7, 8, 9, 10, 11, 12, 38],       #Hair and Makeup
+    3: [13, 14, 15, 16, 17, 18, 39],    #Photo and Video
+    4: [19, 20, 21, 22, 23, 24, 40],    #Designing
+    5: [25, 26, 27, 28, 29, 30, 41],    #Entertainment
+    6: [31, 32, 33, 34, 35, 36, 42],    #Coordination
 }
 
 package_departments = {
@@ -50,7 +50,7 @@ for _ in range(num_samples):
     dataset.append(project)
 
 # Save updated dataset to JSON
-dataset_path = "synthetic_project_dataset.json"
+dataset_path = "datasets\synthetic_project_dataset.json"
 with open(dataset_path, "w") as f:
     json.dump(dataset, f, indent=4)
 
