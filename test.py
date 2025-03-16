a6806d71f6c58d2faab90f93b2cb83f6b35e3913
sezilla <chthollyc@gmail.com>
2025-03-16 16:39:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import os
+import sys
 from openai import OpenAI
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from src.config import GITHUB_TOKEN, MODEL_NAME
 
 client = OpenAI(
     base_url="https://models.inference.ai.azure.com",
-    api_key=os.getenv["GITHUB_TOKEN"],
+    api_key=GITHUB_TOKEN,
 )
 
 response = client.chat.completions.create(
@@ -17,7 +21,7 @@ response = client.chat.completions.create(
             "content": "What is the capital of France?",
         }
     ],
-    model="gpt-4o",
+    model=MODEL_NAME,
     temperature=1,
     max_tokens=4096,
     top_p=1
